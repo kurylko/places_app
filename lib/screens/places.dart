d662a2ae1e5dab4ec33be7544852cfd7207105de
kurylko <reviliya@gmail.com>
2023-12-15 15:30:33 +0100

    provider works

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:places_app/providers/user_places.dart';
 import 'package:places_app/screens/add_place.dart';
 import 'package:places_app/widgets/places_list.dart';
 
-class PlacesScreen extends StatelessWidget {
+class PlacesScreen extends ConsumerWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your places'),
@@ -24,7 +29,7 @@ class PlacesScreen extends StatelessWidget {
         ],
       ),
       body: PlacesList(
-        places: [],
+        places: userPlaces,
       ),
     );
   }
