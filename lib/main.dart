8fba49feafc0ff538422bb82bb5f717e5dbd608a
kurylko <reviliya@gmail.com>
2023-12-15 13:55:11 +0100

    places screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:places_app/screens/places.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Text('Flutter Demo Home Page'),
+      home: const PlacesScreen(),
     );
   }
 }
