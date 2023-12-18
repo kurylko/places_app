f78440a8068c116b61cbf2a8ca75990d8153bba2
kurylko <reviliya@gmail.com>
2023-12-18 17:54:59 +0100

    added location picker package

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:places_app/providers/user_places.dart';
 import 'package:places_app/widgets/image_input.dart';
+import 'package:places_app/widgets/location_input.dart';
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
@@ -61,6 +62,10 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 _selectedImage = image;
               },
             ),
+            const SizedBox(
+              height: 16,
+            ),
+            LocationInput(),
             const SizedBox(
               height: 16,
             ),
